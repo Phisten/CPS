5045ea3d33681e39cdc120b015d096814eff11e0
csic018548 <csic018548@gmail.com>
2017-06-28 21:42:39 +0800

    色卡切割
    t1: picBox2輸出直方圖後可以透過UI直接輸入色相帶通範圍
    t2: picbox4,5 輸出HSV的S和V
    t3: picBox6切割出色卡極小部分白邊用於最後距離與角度的計算

--- WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
@@ -32,12 +32,20 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.button3 = new System.Windows.Forms.Button();
             this.chkConvertHSV = new System.Windows.Forms.CheckBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.tbHueRangeLeft = new System.Windows.Forms.TextBox();
+            this.tbHueRangeRight = new System.Windows.Forms.TextBox();
+            this.btnHueInRange = new System.Windows.Forms.Button();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -82,16 +90,6 @@
             this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseMove);
             this.pictureBox2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseUp);
             // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(641, 299);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(114, 49);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "影像處理(二值化)";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
             // chkConvertHSV
             // 
             this.chkConvertHSV.AutoSize = true;
@@ -112,24 +110,82 @@
             this.pictureBox3.TabIndex = 6;
             this.pictureBox3.TabStop = false;
             // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(1263, 12);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(462, 324);
+            this.pictureBox4.TabIndex = 7;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(1263, 342);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(462, 324);
+            this.pictureBox5.TabIndex = 8;
+            this.pictureBox5.TabStop = false;
+            // 
+            // tbHueRangeLeft
+            // 
+            this.tbHueRangeLeft.Location = new System.Drawing.Point(612, 443);
+            this.tbHueRangeLeft.Name = "tbHueRangeLeft";
+            this.tbHueRangeLeft.Size = new System.Drawing.Size(100, 22);
+            this.tbHueRangeLeft.TabIndex = 9;
+            // 
+            // tbHueRangeRight
+            // 
+            this.tbHueRangeRight.Location = new System.Drawing.Point(612, 485);
+            this.tbHueRangeRight.Name = "tbHueRangeRight";
+            this.tbHueRangeRight.Size = new System.Drawing.Size(100, 22);
+            this.tbHueRangeRight.TabIndex = 10;
+            // 
+            // btnHueInRange
+            // 
+            this.btnHueInRange.Location = new System.Drawing.Point(718, 460);
+            this.btnHueInRange.Name = "btnHueInRange";
+            this.btnHueInRange.Size = new System.Drawing.Size(61, 36);
+            this.btnHueInRange.TabIndex = 11;
+            this.btnHueInRange.Text = "關";
+            this.btnHueInRange.UseVisualStyleBackColor = true;
+            this.btnHueInRange.Click += new System.EventHandler(this.btnHueInRange_Click);
+            this.btnHueInRange.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnHueInRange_MouseClick);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(12, 471);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(293, 263);
+            this.pictureBox6.TabIndex = 12;
+            this.pictureBox6.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1417, 656);
+            this.ClientSize = new System.Drawing.Size(1749, 707);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.btnHueInRange);
+            this.Controls.Add(this.tbHueRangeRight);
+            this.Controls.Add(this.tbHueRangeLeft);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.chkConvertHSV);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -141,9 +197,14 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.CheckBox chkConvertHSV;
         private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.TextBox tbHueRangeLeft;
+        private System.Windows.Forms.TextBox tbHueRangeRight;
+        private System.Windows.Forms.Button btnHueInRange;
+        private System.Windows.Forms.PictureBox pictureBox6;
     }
 }
 
--- WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -11,6 +11,7 @@ using Emgu.CV;
 using Emgu.CV.Structure;
 using Emgu.Util;
 using Emgu.CV.UI;
+using Emgu.CV.Cvb;
 
 
 
@@ -19,19 +20,22 @@ namespace WindowsFormsApplication2
     public partial class Form1 : Form
     {
         Image<Bgr, Byte> frame;
+        Gray hueInRangeLeft = new Gray(0d), hueInRangeRight = new Gray(0d);
+
+
         public Form1()
         {
             InitializeComponent();
-            frame = new Image<Bgr, byte>(new Size(640,480));
+            frame = new Image<Bgr, byte>(new Size(640, 480));
         }
         private Capture cap = null;
 
         private void Form1_Load(object sender, EventArgs e)
         {
             //cap = new Capture(0);
-           // Application.Idle += new EventHandler(Application_Idle);
+            // Application.Idle += new EventHandler(Application_Idle);
         }
-       
+
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -46,40 +50,91 @@ namespace WindowsFormsApplication2
             Application.Idle -= new EventHandler(Application_Idle);//清除idle的值
             pictureBox1.Image = null;//清除pictureBox1的圖
 
-            
+
         }
-        
+
         private void button3_Click(object sender, EventArgs e)
         {
-            
+
         }
         void Application_Idle(object sender, EventArgs e)
         {
+            //由相機載入影像
             frame = cap.QueryFrame();
             Bitmap b1 = frame.ToBitmap();
             Graphics g = Graphics.FromImage(b1);
             g.DrawRectangle(new Pen(Color.Red, 2.0f), SrcImgRoiRect);
             pictureBox1.Image = b1;
+
+            //根據ROI(相機可能會拍到色卡的區域) 切割影像
             frame.ROI = SrcImgRoiRect;
             Image<Hsv, byte> roiimg = frame.Convert<Hsv, byte>();
-            Image<Gray, byte> HueImg = roiimg[0];
+            Image<Gray, byte> HueImg = roiimg[0]; //H
+            pictureBox4.Image = roiimg[1].ToBitmap(); //S
+            pictureBox5.Image = roiimg[2].ToBitmap(); //V
             if (chkConvertHSV.Checked)
             {
-                roiimg = HueImg.Convert<Hsv, byte>();
+                pictureBox2.Image = HueImg.Convert<Hsv, byte>().ToBitmap();
             }
-            pictureBox2.Image = roiimg.ToBitmap();
+            else
+            {
+                pictureBox2.Image = roiimg.ToBitmap();
+            }
+            
             g = Graphics.FromImage(pictureBox2.Image);
             g.DrawRectangle(new Pen(Color.Red, 2.0f), HueImgRoiRect);
 
-
-            Image<Gray, byte> HueCOI = HueImg.InRange(new Gray(5 - 5), new Gray(5 + 5));
+            //根據目前要搜索的顏色二值化影像
+            Image<Gray, byte> HueCOI = HueImg.InRange(hueInRangeLeft, hueInRangeRight);
+            //Image<Gray, byte> erodeimage = HueCOI.Erode(1);
+            //Image<Gray, byte> dilateimage = erodeimage.Dilate(1);
             pictureBox3.Image = HueCOI.ToBitmap();
+
+            
+            //根據COI二值化影像找出最大色塊
+            CvBlob maxBlob = null;
+            CvBlobDetector m_blobDetector = new CvBlobDetector();
+            CvBlobs blobs = new CvBlobs();
+            m_blobDetector.Detect(HueCOI, blobs);
+            if (blobs.Count > 0)
+            {
+                maxBlob = blobs.ElementAt(0).Value;
+                foreach (var item in blobs)
+                {
+                    if (item.Value.Area > maxBlob.Area)
+                    {
+                        maxBlob = item.Value;
+                    }
+                }
+            }
+            
+            if (maxBlob != null )
+            {
+                Rectangle tmpRect = maxBlob.BoundingBox;
+                int whiteWidth = tmpRect.Width / 8;
+                int whiteHeight = tmpRect.Height / 8;
+
+                tmpRect.X = Math.Max(0, tmpRect.X - whiteWidth);
+                tmpRect.Y = Math.Max(0, tmpRect.Y - whiteHeight);
+                tmpRect.Width = Math.Min(roiimg.Width - tmpRect.X, tmpRect.Width + whiteWidth * 2);
+                tmpRect.Height = Math.Min(roiimg.Height - tmpRect.Y, tmpRect.Height + whiteHeight * 2);
+                roiimg.ROI = tmpRect;
+                
+                Image<Hsv, byte> targetROI = roiimg.Copy();
+
+                pictureBox6.Image = targetROI.ToBitmap() ;
+            }
+
+
 
 
 
         }
-        Point SrcImgRoiP1= new Point(0, 0), SrcImgRoiP2= new Point(0, 0);
-        Rectangle SrcImgRoiRect=new Rectangle(0,0,0,0);
+
+
+
+        Point SrcImgRoiP1 = new Point(0, 0), SrcImgRoiP2 = new Point(0, 0);
+        Rectangle SrcImgRoiRect = new Rectangle(0, 0, 0, 0);
         bool setSrcImgRoiRect = false;
         private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
         {
@@ -96,28 +151,28 @@ namespace WindowsFormsApplication2
             {
                 SrcImgRoiP2.X = e.X;
                 SrcImgRoiP2.Y = e.Y;
-                if (SrcImgRoiP2.X>SrcImgRoiP1.X &&SrcImgRoiP2.Y>SrcImgRoiP1.Y)
+                if (SrcImgRoiP2.X > SrcImgRoiP1.X && SrcImgRoiP2.Y > SrcImgRoiP1.Y)
                 {
                     SrcImgRoiRect.X = SrcImgRoiP1.X;
                     SrcImgRoiRect.Y = SrcImgRoiP1.Y;
                     SrcImgRoiRect.Width = SrcImgRoiP2.X - SrcImgRoiP1.X;
-                    SrcImgRoiRect.Height = SrcImgRoiP2.Y - SrcImgRoiP1.Y;       
+                    SrcImgRoiRect.Height = SrcImgRoiP2.Y - SrcImgRoiP1.Y;
                 }
-                else if (SrcImgRoiP2.X>SrcImgRoiP1.X &&SrcImgRoiP2.Y<SrcImgRoiP1.Y)
+                else if (SrcImgRoiP2.X > SrcImgRoiP1.X && SrcImgRoiP2.Y < SrcImgRoiP1.Y)
                 {
                     SrcImgRoiRect.X = SrcImgRoiP1.X;
                     SrcImgRoiRect.Y = SrcImgRoiP2.Y;
                     SrcImgRoiRect.Width = SrcImgRoiP2.X - SrcImgRoiP1.X;
-                    SrcImgRoiRect.Height = SrcImgRoiP1.Y - SrcImgRoiP2.Y;   
+                    SrcImgRoiRect.Height = SrcImgRoiP1.Y - SrcImgRoiP2.Y;
                 }
-                else if (SrcImgRoiP2.X<SrcImgRoiP1.X &&SrcImgRoiP2.Y>SrcImgRoiP1.Y)
+                else if (SrcImgRoiP2.X < SrcImgRoiP1.X && SrcImgRoiP2.Y > SrcImgRoiP1.Y)
                 {
                     SrcImgRoiRect.X = SrcImgRoiP2.X;
                     SrcImgRoiRect.Y = SrcImgRoiP1.Y;
                     SrcImgRoiRect.Width = SrcImgRoiP1.X - SrcImgRoiP2.X;
-                    SrcImgRoiRect.Height = SrcImgRoiP2.Y - SrcImgRoiP1.Y;  
+                    SrcImgRoiRect.Height = SrcImgRoiP2.Y - SrcImgRoiP1.Y;
                 }
-                else if (SrcImgRoiP2.X<SrcImgRoiP1.X &&SrcImgRoiP2.Y<SrcImgRoiP1.Y)
+                else if (SrcImgRoiP2.X < SrcImgRoiP1.X && SrcImgRoiP2.Y < SrcImgRoiP1.Y)
                 {
                     SrcImgRoiRect.X = SrcImgRoiP2.X;
                     SrcImgRoiRect.Y = SrcImgRoiP2.Y;
@@ -188,10 +243,25 @@ namespace WindowsFormsApplication2
 
             DenseHistogram hueHist = new DenseHistogram(256, new RangeF(0, 255));
             //參數一是要計算的顏色資料，這邊分割通道並取得Blue通道的顏色;依序[1]:Green->[2]:Red
-            hueHist.Calculate(new IImage[] { new Image<Gray,byte>((Bitmap)pictureBox2.Image) }, false, null);
+            hueHist.Calculate(new IImage[] { new Image<Gray, byte>((Bitmap)pictureBox2.Image) }, false, null);
             HistogramViewer.Show(hueHist, "hue channel");
 
         }
-       
+
+        private void btnHueInRange_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void btnHueInRange_Click(object sender, EventArgs e)
+        {
+            double tmp;
+            double.TryParse(tbHueRangeRight.Text, out tmp);
+            hueInRangeRight = new Gray(tmp);
+            double.TryParse(tbHueRangeLeft.Text, out tmp);
+            hueInRangeLeft = new Gray(tmp);
+
+        }
+
     }
 }
